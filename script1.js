81e559e01ba1e2a1a43c6d45139989323c65bec1
leahtorres <leah_torres@davincischools.org>
2019-03-11 22:11:00 -0700

    Update script1.js

--- script1.js
+++ script1.js
@@ -207,7 +207,7 @@ var downloadTimer = setInterval(function(){
     clearInterval(downloadTimer);
 }, 1000);
 	setTimeout(function(){ alert("You ran out of time!!"); }, 41000);
-		window.location.reload(true);
+		//window.location.reload(true);
 	
 
 
